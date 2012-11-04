94ca28881e548b6694cec15d828531f8f6d467ea
yspxman@gmail.com <yspxman@gmail.com>
2012-11-04 20:54:21 +0800

    init

---
+++ MvvmLightDemo/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,68 @@
+﻿/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocatorTemplate xmlns:vm="using:ProjectForTemplates.ViewModel"
+                                   x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+*/
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using MvvmLightDemo.Model;
+
+namespace MvvmLightDemo.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            // SetLocatorProvider 接受一个delegate？why， interface 的delegate??
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            }
+            else
+            {
+                SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Main property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel Main
+        {
+            //？？ 这个main 属性是啥? 谁在调用？或许和xmal有关?
+            get
+            {
+                // ServiceLocator.Current 返回 currentProvider， 在这里就是SimpleIoc, simpleIoc 实现了 IServiceLocator
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+        }
+    }
+}
